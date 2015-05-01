3f0a8d1ee904a5a1665df410c843c28fe3d87467
Chris Mangan <me@example.com>
2015-04-30 20:50:48 -0500

    Add initialMarkers array and for...in loop to populate on map.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 function AppViewModel() {
-    this.firstName = ko.observable("Bert");
-    this.lastName = ko.observable("Bertington");
+    this.searchBox = ko.observable("");
 }
+
+
+/* Model */
+//List of initial map items
+var initialMarkers = ko.observableArray([
+	{
+		name: 'McDonalds',
+		lat: 37.975585,
+		lng: -100.853316
+	},
+	{
+		name: 'Golden Corral',
+		lat: 37.97548,
+		lng: -100.851009
+	}
+
+	]);
 
 ko.applyBindings(new AppViewModel());
--- js/google-maps.js
+++ js/google-maps.js
@@ -1,18 +1,36 @@
 function initialize() {
-        var mapOptions = {
-          center: { lat: 37.969451, lng: -100.836252},
-          zoom: 17
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
+      var myLatlng = new google.maps.LatLng(37.97564, -100.850363);
+      var mapOptions = {
+          zoom: 17,
+        center: myLatlng
+      };
+
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+          mapOptions);
+      //Retrieve initial markers from app.js
+      for (item in initialMarkers()) {
+        var marker = new google.maps.Marker({
+          title: initialMarkers()[item].name,
+          position: {
+            lat: initialMarkers()[item].lat, 
+            lng: initialMarkers()[item].lng
+          },
+          map: map
+        })
       }
-      google.maps.event.addDomListener(window, 'load', initialize);
 
-function loadScript() {
-	var script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAc52EIkHNkfufX9x6ZW2iIv6KD83WZuh4';
+      /*var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(37.975585, -100.853316),
+          map: map,
+          title: 'McDonalds'
+      }); */
+    };
+    function loadScript() {
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' + '&signed_in=true&callback=initialize';
       document.body.appendChild(script);
-}
+    }
+  
 
-window.onload = loadScript;
+    window.onload = loadScript;
